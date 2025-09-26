09cdf2d29c3fa6797e4081253154358b1b1e061b
CENTIC <CENTIC@10.6.65.41>
2025-09-26 11:29:37 -0500

    Rama1.1

--- src/Booking/Bus.java
+++ src/Booking/Bus.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Booking;
 
-/**
- *
- * @author CENTIC
- */
-public class Bus {
+
+public class Bus extends TransportService{
+    
+    private String busCompany;
+    private String boardingPoint;
+
+    public Bus() {
+    }
+
+    public Bus(String busCompany, String boardingPoint) {
+        this.setBusCompany(busCompany);
+        this.setBoardingPoint(boardingPoint);
+    }
+    
+    public void setBusCompany(String busCompany) {
+        this.busCompany = busCompany;
+    }
+    public void setBoardingPoint(String boardingPoint) {
+        this.boardingPoint = boardingPoint;
+    }
+
+    public String getBusCompany() {
+        return busCompany;
+    }
+    
+    public String getBoardingPoint() {
+        String str = "\n" + this.getId + "has reached the point of embarking";
+        return str;
+    }
+    public String getServiceInformation(){
+        String str = "\n" + this.busCompany
+        return 
+    }
+    
+    @Override
+    public String toString() {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
     
 }
