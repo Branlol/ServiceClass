13551f9af6c8a6e16a16da76964769d533c0edbf
CENTIC <CENTIC@10.6.65.125>
2025-09-26 11:02:19 -0500

    Actualización 1.2

--- src/bookingplatform/BookingPlatform.java
+++ src/bookingplatform/BookingPlatform.java
@@ -3,9 +3,6 @@ package bookingplatform;
 
 public class BookingPlatform {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
