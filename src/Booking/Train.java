6298f3d6feaf90af8c71c9bb8566bb698b98aaf9
CENTIC <CENTIC@10.6.65.83>
2025-09-26 11:26:41 -0500

    rama3.1

--- src/Booking/Train.java
+++ src/Booking/Train.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Booking;
 
-/**
- *
- * @author CENTIC
- */
-public class Train {
+public class Train extends TransportService{
+    private String seatType;
+    private int wagonNumber;
     
+    public Train(){
+        
+    }
+    public Train (int id, String origin, String destinaton, double price, String seatType, int wagonNumber){
+        
+        this.setSeatType(seatType);
+        this.setWagonNumber(wagonNumber);
+    }
+
+    public String getSeatType() {
+        return seatType;
+    }
+
+    public final void setSeatType(String seatType) {
+        this.seatType = seatType;
+    }
+
+    public int getWagonNumber() {
+        return wagonNumber;
+    }
+
+    public final void setWagonNumber(int wagonNumber) {
+        this.wagonNumber = wagonNumber;
+    }
+    
+    public String getServiceInformation(){
+        return null;
+    }
+    
+    public String getLuggageInformation(){
+        return null;
+    }
+    
+    @Override
+    public String toString(){
+        return null;
+    }
 }
--- src/bookingplatform/BookingPlatform.java
+++ src/bookingplatform/BookingPlatform.java
@@ -4,7 +4,7 @@ package bookingplatform;
 public class BookingPlatform {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
